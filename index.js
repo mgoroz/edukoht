3afccea73e643bbb5bc0edf84288d21b74a085c8
Maksim Gorozhanko <Polotenchik10>
2023-04-21 16:50:40 +0300

    initial push

---
+++ index.js
@@ -0,0 +1,16 @@
+// Connect to server using WebSocket
+const ws = new WebSocket("ws://localhost:3000");
+
+// Listen for incoming messages
+ws.onmessage = function(event) {
+	const message = event.data;
+	addMessageToList(message);
+};
+
+// Add a message to the list
+function addMessageToList(message) {
+	const messageList = document.getElementById("message-list");
+	const li = document.createElement("li");
+	li.innerText = message;
+	messageList.appendChild(li);
+}
